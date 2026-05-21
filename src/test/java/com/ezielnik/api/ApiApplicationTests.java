20b816dd68aa9778bf516be77be3a6dac4c1ec22
maksym456 <maksym.wilk@studenci.collegiumwitelona.pl>
2026-05-21 03:05:14 +0200

    Fixes preventing build

--- src/test/java/com/ezielnik/api/ApiApplicationTests.java
+++ src/test/java/com/ezielnik/api/ApiApplicationTests.java
@@ -3,7 +3,9 @@ package com.ezielnik.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "jwt.secret=test-secret-key-for-unit-tests-only-must-be-at-least-32-characters-long"
+})
 class ApiApplicationTests {
 
     @Test
